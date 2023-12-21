ce00009815b1029be080f705f0271bd6780e5dc7
Sergiu <soroceansergiu10@gmail.com>
2023-12-21 22:55:19 +0200

    Add project files.

---
+++ Shared_Recipes/Models/Recipes.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared_Recipes.Models
+{
+    public class Recipes
+    {
+        [Key]
+        private int id { get; set;}
+        public string Title { get; set; }
+    }
+}
